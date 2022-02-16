8be2b57803d36d26806f111f7f2eb4a5951441ba
PeterFonkel <peterfonkel@gmail.com>
2022-02-16 12:41:11 +0100

    update

--- src/main/java/com/peterfonkel/ecommerce/EcommerceApplication.java
+++ src/main/java/com/peterfonkel/ecommerce/EcommerceApplication.java
@@ -3,6 +3,7 @@ package com.peterfonkel.ecommerce;
 import org.springframework.boot.SpringApplication;
 
 
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
